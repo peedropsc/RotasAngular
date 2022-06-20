22894e8e2ef735bd174204ac1530e184a85d8920
Pedro Paulo <93000431+peedropsc@users.noreply.github.com>
2022-06-20 09:12:29 -0300

    Add files via upload

---
+++ ProjectMain/app/controllers/controller.js
@@ -0,0 +1,11 @@
+app.controller('HomeCtrl', function($scope) {
+    $scope.title = 'Bem-vindo, esta é a página principal!';
+});
+
+app.controller('ContatoCtrl', function($scope) {
+    $scope.title = 'Entre em contato!';
+});
+
+app.controller('SobreCtrl', function($scope) {
+    $scope.title = 'Olá, aqui falaremos sobre a empresa!';
+});
